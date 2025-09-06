7b6901c12d2f809a33f15a3460d6c4e49836f789
Maninder Singh <makkusidhu@yahoo.com>
2025-09-06 17:16:33 +0200

    Updated Seabox branding and added local fonts

--- src/data/services.js
+++ src/data/services.js
@@ -1,5 +1,5 @@
 export const services = [
   { id:'ftl', img:'src/data/Images/seabox41.jpeg' },
-  { id:'ltl', img:'src/data/Images/seabox51.jpeg' },
+  { id:'ltl', img:'src/data/Images/seabox31.jpeg' },
   { id:'flatbed', img:'src/data/Images/seabox61.jpeg' }
 ]
